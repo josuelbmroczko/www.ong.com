794a2a389f099ec895e29fd8fe840fc4ef96977a
josuelbmroczko <josuelbeserramroczko@gmail.com>
2024-05-17 16:19:26 -0300

    a

--- src/componentes/blog/blog1/blog1.jsx
+++ src/componentes/blog/blog1/blog1.jsx
@@ -2,14 +2,35 @@ import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import imagem1 from '../imagensBlog/imagem1.png';
 import imagem2 from '../imagensBlog/imagem2.png';
-import imagem3 from '../imagensBlog/imagem3.png';
+import imagem3 from '../imagensBlog/imagem11.png';
 import imagem4 from '../imagensBlog/imagem4.png';
 import imagem5 from '../imagensBlog/imagem5.png';
 import imagem6 from '../imagensBlog/imagem6.png';
 import imagem7 from '../imagensBlog/imagem7.png';
 import imagem8 from '../imagensBlog/imagem8.png';
 import imagem9 from '../imagensBlog/imagem9.png';
+
 import imagem10 from '../imagensBlog/imagem10.png';
+import imagem11 from '../imagensBlog/imagem11.png';
+import imagem12 from '../imagensBlog/imagem12.png';
+import imagem13 from '../imagensBlog/imagem13.png';
+import imagem14 from '../imagensBlog/imagem14.png';
+import imagem15 from '../imagensBlog/imagem15.png';
+import imagem16 from '../imagensBlog/imagem16.png';
+import imagem17 from '../imagensBlog/imagem17.png';
+import imagem18 from '../imagensBlog/imagem18.png';
+import imagem19 from '../imagensBlog/imagem19.png';
+ 
+import imagem21 from '../imagensBlog/imagem21.png';
+import imagem22 from '../imagensBlog/imagem22.png';
+import imagem23 from '../imagensBlog/imagem23.png';
+import imagem24 from '../imagensBlog/imagem24.png';
+import imagem25 from '../imagensBlog/imagem25.png';
+import imagem26 from '../imagensBlog/imagem26.png';
+ 
+
+ 
+
 import { Container } from '../styles';
 import Home from '../../home';
 import Footer from '../../footer/footer';
@@ -27,10 +48,10 @@ const CarouselContainer = styled.div`
  
   &::-webkit-scrollbar {
     width: 100%; /* Largura do scrollbar */
-    height: 3px;
+    height: 8px;
   }
   &::-webkit-scrollbar-thumb {
-    background-color: #888; /* Cor do scrollbar */
+    background-color: #4a56c0; /* Cor do scrollbar */
   }
 `;
 
@@ -70,7 +91,7 @@ const NextButton = styled(Button)`
 `;
 
 export default function Blog1() {
-  const images = [imagem1, imagem2, imagem3, imagem4, imagem5, imagem6, imagem7, imagem8, imagem9, imagem10];
+  const images = [imagem1, imagem2, imagem3, imagem4, imagem5, imagem6, imagem7, imagem8, imagem9,imagem10,imagem11,imagem12,imagem13,imagem14,imagem15,imagem16,imagem17,imagem18,imagem19,imagem21,imagem22,imagem23,imagem24,imagem25,imagem26,];
   const containerRef = useRef(null);
   const [currentIndex, setCurrentIndex] = useState(0);
   const scrollAmount = 300; // Ajuste este valor conforme necessário

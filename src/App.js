82d8800a91d3c40e9ce97bf97681033b6cc6195d
josuelbmroczko <josuelbeserramroczko@gmail.com>
2024-05-24 13:31:27 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homeeapp from './pages/home/index.jsx';
-  import { ImageProvider } from '../src/imagemProvider/imagemProvider.jsx';
- 
-import './App.css'
- import { Helmet } from 'react-helmet';
+import { ImageProvider } from '../src/imagemProvider/imagemProvider.jsx';
+import './App.css';
+import { Helmet } from 'react-helmet';
 import Blog1 from './componentes/blog/blog1/blog1.jsx';
 import Blog2 from './componentes/blog/blog2/blog2.jsx';
 import Blog3 from './componentes/blog/blog3/blog3.jsx';
@@ -13,40 +12,52 @@ import DoacaoQrCode from './componentes/botaoDoeQrCode/doe.jsx';
 import SobreConteudo from './pages/sobre/sobreConteudo.jsx';
 import ContatoConteudo from './pages/contatos/contatoConteudo.jsx';
 import Projetos from './componentes/projtos/projetos.jsx';
+import logo from './imagem/logo1.png';  // Verifique se o caminho está correto
 
- 
-    
-
+function Loading() {
+  return (
+    <div className="loading">
+      <img src={logo} alt="Logo" /> {/* Adicione a propriedade alt para acessibilidade */}
+      <h1>Projeto Geração Resgate</h1> {/* Adicionando o texto de boas-vindas */}
+    </div>
+  );
+}
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <ImageProvider>
-            <Helmet>
-          <meta name="description" content='ong' />
-          <meta name="description" content='ong ' />
-          <meta name="description" content='ong ' />
-
-          </Helmet>
+      <Helmet>
+        <meta name="description" content="ong" />
+        <meta name="description" content="ong " />
+        <meta name="description" content="ong " />
+      </Helmet>
       <Router>
         <Routes>
-          {/* 
-  Esta é a rota para a página inicial.
-  Quando a URL corresponder a "/", o componente Homeeapp será renderizado.
-*/}
           <Route path="/" element={<Homeeapp />} />
-           <Route path="/contato" element={<ContatoConteudo />} />
+          <Route path="/contato" element={<ContatoConteudo />} />
           <Route path="/sobre" element={<SobreConteudo />} />
-          <Route path="/doe" element={<DoacaoQrCode/>}/>
-
+          <Route path="/doe" element={<DoacaoQrCode />} />
           <Route path="/nova1" element={<Blog1 />} />
           <Route path="/nova2" element={<Blog2 />} />
           <Route path="/nova3" element={<Blog3 />} />
           <Route path="/nova4" element={<Blog4 />} />
-          <Route path='/projetos' element={<Projetos/>}/>
-           
-         </Routes> 
+          <Route path="/projetos" element={<Projetos />} />
+        </Routes>
       </Router>
-
     </ImageProvider>
   );
 }
